3c590cea0fa5dde74626ff2b1f0a3aa58179654a
Briana <bsisler81@gmail.com>
2025-11-05 14:34:23 -0500

    Created new branch; changed Hello World print, added second print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Briana!");
+        System.out.println("Briana's New Line!!");
     }
 }
